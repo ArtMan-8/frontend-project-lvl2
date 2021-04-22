c0d2ba1eb7832f14689ec8a00a34699ff4f00615
Art <ya.Artriches@ya.ru>
2021-04-22 21:25:55 +0300

    add: complex fixtures

--- __tests__/src/index.test.js
+++ __tests__/src/index.test.js
@@ -10,17 +10,17 @@ const getFixturePath = (filename) => path.join(__dirname, '../..', '__fixtures__
 
 describe('genDiff from simple file', () => {
   test('json', () => {
-    const file1 = getFixturePath('file1.json');
-    const file2 = getFixturePath('file2.json');
-    const diff = readFile(getFixturePath('diff.txt'));
+    const file1 = getFixturePath('simple-file-1.json');
+    const file2 = getFixturePath('simple-file-2.json');
+    const diff = readFile(getFixturePath('simple-file-diff-stylish.txt'));
 
     expect(genDiff(file1, file2)).toBe(diff);
   });
 
   test('yaml', () => {
-    const file1 = getFixturePath('file1.yml');
-    const file2 = getFixturePath('file2.yaml');
-    const diff = readFile(getFixturePath('diff.txt'));
+    const file1 = getFixturePath('simple-file-1.yml');
+    const file2 = getFixturePath('simple-file-2.yaml');
+    const diff = readFile(getFixturePath('simple-file-diff-stylish.txt'));
 
     expect(genDiff(file1, file2)).toBe(diff);
   });
